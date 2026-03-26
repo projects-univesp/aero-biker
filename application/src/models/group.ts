a38768047ece4bea8cb1cea388f2ce9c64b0921a
Jorge Martins Jr <jorgemartinsjr@proton.me>
2026-03-26 20:52:44 -0300

    feat: add group model, validation and service

---
+++ application/src/models/group.ts
@@ -0,0 +1,46 @@
+import { sequelize } from "@config/database";
+import { UUID } from "crypto";
+import { DataTypes, Model } from "sequelize";
+
+export class Group extends Model {
+  declare id: UUID;
+  declare name: string;
+  declare maxCapacity: number;
+  declare daysOfWeek: string;
+  declare time: string;
+  declare isActive: boolean;
+}
+
+Group.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    maxCapacity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    daysOfWeek: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "Groups",
+  }
+);
