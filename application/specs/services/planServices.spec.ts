cc20cf5f291255a2ecbc0240bfa57a757d804c99
fable00 <saymonmcedo00@gmail.com>
2026-04-10 15:38:41 -0300

    fix: fix test structure back to pattern

--- application/specs/services/planServices.spec.ts
+++ application/specs/services/planServices.spec.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { describe, it, expect } from "vitest";
-import { app } from "../app";
+import { app } from "../../src/app";
 
 describe("Plan API", () => {
   let createdPlanId: string;
