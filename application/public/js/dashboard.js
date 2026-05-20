0e58ad1b39457f00025f462671f3f98d8dc77d2e
aleciolr <aleciolucasr@gmail.com>
2026-05-20 18:04:48 -0300

    feat/fix: implementação da página de Dashboard

--- application/public/js/dashboard.js
+++ application/public/js/dashboard.js
@@ -1,10 +1,19 @@
+const formatDate = () => {
+  const date = new Date();
+  const formmatter = new Intl.DateTimeFormat("pt-BR", {
+    month: "2-digit",
+    year: "numeric",
+  });
+  return formmatter.format(date);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const studentRows = document.querySelectorAll(".student-row");
   const groupRows = document.querySelectorAll(".group-row");
   const planRows = document.querySelectorAll(".plan-row");
   const paymentRows = document.querySelectorAll(".payment-row");
-
   const dashboardCounters = document.getElementById("dashboard-counters");
+  const dashboardSubtitle = document.getElementById("dashboard-subtitle")
 
   const activeGroupsCount = Array.from(groupRows).filter(
     (row) => row.getAttribute("data-status") === "active",
@@ -16,5 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (dashboardCounters) {
     dashboardCounters.textContent = `${studentRows.lenght} alunos * ${activeGroupsCount} planos ativos * ${paymentRows.lenght} pagamentos`;
+  }
+  if (dashboardSubtitle) {
+    dashboardSubtitle.textContent = `Visão Geral — ${formatDate()}`;
   }
 })
